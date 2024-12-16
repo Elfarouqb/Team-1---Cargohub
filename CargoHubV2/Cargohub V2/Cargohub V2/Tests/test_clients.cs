84367c987a24e1bb97950694c9ce7d2e8e2fe1e7
nhojW <161321640+nhojW@users.noreply.github.com>
2024-12-16 17:44:55 +0100

    Fixed code, and path, added tests and fixed code checks

--- CargoHubV2/Cargohub V2/Cargohub V2/Tests/test_clients.cs
+++ CargoHubV2/Cargohub V2/Cargohub V2/Tests/test_clients.cs
@@ -1,15 +1,15 @@
-// The 'using' statement for Test Tools is in GlobalUsings.cs
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// // The 'using' statement for Test Tools is in GlobalUsings.cs
+// // using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Cargohub_V2.Tests
+// namespace Cargohub_V2.Tests
 
-{
-    [TestClass]
-    public class ClientsTest
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
+// {
+//     [TestClass]
+//     public class ClientsTest
+//     {
+//         [TestMethod]
+//         public void TestMethod1()
+//         {
+//         }
+//     }
+// }
